48de64dbc4d53c261b79dc6c2a409138a391cf47
Kevin Pereira <kevinpsgp@gmail.com>
2025-11-26 21:48:55 -0300

    Primeiro commit: Adiciona a solucao do FizzBuzz

---
+++ FizzBuzz.js
@@ -0,0 +1,33 @@
+let index = 0;
+
+function divideBy3(index){
+  return index % 3 === 0;
+}
+
+function divideBy5(index){
+    return index % 5 === 0;
+}
+
+function divideBy3and5(index){
+   return divideBy3(index) && divideBy5(index);
+}
+
+function check(number){
+
+    if (divideBy3and5(number)) {
+        console.log("Number: [ "+ number + " ] FizzBuzz");
+    }
+    else if(divideBy3(number)) {
+        console.log("Number: [ "+ number + " ] Fizz");
+    } 
+    else if(divideBy5(number)){
+        console.log("Number: [ "+ number + " ] Buzz");
+    }
+    else{
+        console.log("Number: [ " + number + " ] " + number);
+    }
+}
+
+for (index; index <= 100; index++) {
+    check(index);
+}
